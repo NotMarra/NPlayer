c3c862d06eea808937bc4eb2ba2f1dca59c0156d
NotMarra <marra313cz@gmail.com>
2023-10-06 22:40:52 +0200

    controls div create

--- nplayer.js
+++ nplayer.js
@@ -4,5 +4,12 @@ class NPlayer {
   constructor(selector) {
     this.playerElement = document.querySelector(selector);
     this.playerElement.classList.add("nplayer");
+
+    this.controls = document.createElement("div");
+    this.controls.classList.add("controls");
+    this.playerElement.insertBefore(
+      this.controls,
+      this.playerElement.firstChild
+    );
   }
 }
